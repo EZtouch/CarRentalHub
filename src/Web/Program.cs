cb0f5c7d53d8b1da4b6c70ceaf6d4bb90ac34f25
EZtouch <hashem.jameel@gmail.com>
2017-10-08 03:53:33 +0300

    Re-release major changes
    - Remove third party libraries from the repository.
    
    - Enhance the project sturcture.
    
    - Update bower packages (bootstrap & jquery mainly).

---
+++ src/Web/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore;
+
+namespace EZtouch.CarRentalHub
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
---
+++ src/Web/Services/IEmailSender.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace EZtouch.CarRentalHub.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
