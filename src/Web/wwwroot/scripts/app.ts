5d0b258256d7d74305b4f0aa021b3e5e540582c5
EZtouch <hashem.jameel@gmail.com>
2017-10-27 21:50:10 +0300

    Usage showcase of typescript 🌀
    - Uses the Colors API to remove Color records from the DB & removes it from the view using AJAX.

--- src/Web/wwwroot/scripts/app.ts
+++ src/Web/wwwroot/scripts/app.ts
@@ -1,5 +1,42 @@
-﻿function sayHello() {
-    const compiler = (document.getElementById("compiler") as HTMLInputElement).value;
-    const framework = (document.getElementById("framework") as HTMLInputElement).value;
-    return `Hello from ${compiler} and ${framework}!`;
+﻿// Assign an event listener to the delete buttons
+$(`.delete-btn`).click((deleteRecord) as any);
+
+class ColorRecord {
+    constructor(public id: string, public row: JQuery<HTMLElement>) {
+        console.log(`Created a color record object successfully  👍`);
+    }
+
+    remove(record: ColorRecord = this) {
+        $.ajax({
+            type: `DELETE`,
+            url: `https://localhost:44367/api/Colors/${record.id}`,
+            dataType: `json`,
+            contentType: `application/json`,
+            success() {
+                record.row.remove();
+                console.log(`Successfully removed ${record.id} 👍`);
+            },
+            error(XMLHttpRequest: JQueryXHR, textStatus: string, errorThrown: string) {
+                console.log(`Unable to remove record ${record.id}, sorry 😔`);
+                console.log(XMLHttpRequest);
+                console.log(textStatus);
+                console.log(errorThrown);
+            }
+        });
+    }
+}
+
+function deleteRecord(event: JQueryEventObject): void {
+    console.log(`Executing the deleteRecord function 👍`);
+    const record = new ColorRecord((event.target as HTMLElement).dataset[`id`] as string, $(event.target).parent().parent());
+    record.remove();
+}
+
+// Create an event listener to the deleteAll 
+$(`#delete-all`).click(deleteSelected);
+
+function deleteSelected(): void {
+    for (let item of $(`input[type="checkbox"]:checked`).toArray()) {
+        $(`.delete-btn[data-id=${item.dataset[`id`]}]`).click();
+    }
 }
